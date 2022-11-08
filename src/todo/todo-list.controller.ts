f91d7be0604fd402b866b42bf42e68c47c7aaf5f
artbeard <frolov.web@gmail.com>
2022-11-08 23:14:34 +0600

    Добавлен базовый функционал для запросов из бд

--- src/todo/todo-list.controller.ts
+++ src/todo/todo-list.controller.ts
@@ -1,33 +1,54 @@
-import { Controller, Get, Post, Patch, Delete, Param, Req, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete, Param, Req, Body } from '@nestjs/common';
 import { Request } from 'express'
+
+import { TodoListService } from './todo-list.service'
 
 interface IDoneItemDto{
     complited: boolean
 }
 
+interface ITodoItemDto{
+    component: string,
+    checked: boolean
+}
+
 @Controller('todo')
 export class TodoListController {
 
+    constructor(
+        private todoListService: TodoListService
+    ){}
+
     @Get()
-    getAll(): string {
-        return 'Массив всех пользовательских списков дел'
+    getAll(@Req() request: Request): any {
+        let uid = request?.cookies.uid;
+        //return `Массив всех пользовательских списков дел ${uid}`;
+        return this.todoListService.findAll(uid);
     }
 
     @Get(':list_id')
-    getList(@Param('list_id') list_id: number, @Req() request: Request): string {
-        
-        console.log(request?.cookies);
-        return `Выбранный список дел: ${list_id}`
+    getList(@Param('list_id') list_id: number, @Req() request: Request): any {
+        //request?.cookies.uid
+        return this.todoListService.findOne(list_id);
     }
 
+    
     @Patch(':list_id/item/:item_id')
     completeTodo(
         @Param('list_id') list_id: number,
         @Param('item_id') item_id: number,
         @Body() {complited}: IDoneItemDto )
     {
-        console.log(complited);
         return `Дело ${item_id} в списке ${list_id} ${complited ? 'сделано!' : 'не сделано :('}`
+    }
+
+    @Put(':list_id/item/:item_id')
+    updateTodo(
+        @Param('list_id') list_id: number,
+        @Param('item_id') item_id: number,
+        @Body() todoItemDto: ITodoItemDto )
+    {
+        return `Дело ${item_id} в списке ${list_id} изменено ${JSON.stringify(todoItemDto, null, 4)}`
     }
 
     @Delete(':list_id/item/:item_id')
--- src/todo/todo-list.service.ts
+++ src/todo/todo-list.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ListEntity } from './list.entity';
 
 @Injectable()
-export class TodoListService {}
+export class TodoListService {
+    constructor(
+        @InjectRepository(ListEntity)
+        private ListEntityRepository: Repository<ListEntity>,
+    ){}
+
+    findAll(uid: number):Promise<ListEntity[]> 
+	{	
+		return this.ListEntityRepository.find({
+            where: {
+                userId: uid,
+            },
+			relations: {
+				items: true,
+			},
+		});
+	}
+
+    findOne(list_id: number): Promise<ListEntity> {
+		return this.ListEntityRepository.findOne({
+            where: {
+                id: list_id,
+            },
+			relations: {
+				items: true,
+			},
+        });
+	}
+
+    completedItem(item_id: number)
+    {
+        
+    }
+}
