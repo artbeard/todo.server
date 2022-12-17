92036d29ed76028bc453f8a107a566a4c4f0b389
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-17 15:45:15 +0600

    Убран лишний мусор,  Рефракторинг
    логика формирования объекта для рендерига в отдельном методе

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import {Controller, Render, HttpException, HttpStatus, Get, Res} from '@nestjs/common';
+import {Controller, Render, Get} from '@nestjs/common';
 import {ConverterService} from "./converter.service";
-import {Response} from 'express';
 import {AppService} from './app.service';
 
 
@@ -21,21 +20,18 @@ export class AppController {
      */
     @Get()
     @Render('page/page-content')
-    action_index(@Res() response: Response) {
+    action_index() {
         return new Promise(resolve => {
             this.appService.loadFile('about.md')
                 .then(text => {
-                    const result = {
-                        content: this.converter.convert(text),
-                        title: (this.converter.getMetaData()).title,
-                        breadcrumbs: [
+                    resolve(
+                        this.build_response(text, [
                             {
                                 title: 'Главная',
                                 href: '/'
                             }
-                        ]
-                    };
-                    resolve(result);
+                        ])
+                    );
                 });
         })
     }
@@ -49,10 +45,8 @@ export class AppController {
         return new Promise(resolve => {
             this.appService.loadFile('disclaimer.md')
                 .then(text => {
-                    const result = {
-                        content: this.converter.convert(text),
-                        title: (this.converter.getMetaData()).title,
-                        breadcrumbs: [
+                    resolve(
+                        this.build_response(text, [
                             {
                                 title: 'О проекте',
                                 href: '/'
@@ -60,9 +54,8 @@ export class AppController {
                             {
                                 title: 'Отказ от обязательств',
                             }
-                        ]
-                    };
-                    resolve(result);
+                        ])
+                    );
                 });
         })
     }
@@ -73,55 +66,27 @@ export class AppController {
     @Get('/todo')
     @Render('page/todo-content')
     action_todo() {
+        return {};
+    }
 
-        let content = `---
-Title:    A Sample MultiMarkdown Document
----
-## Description
 
-[Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
-
-## Installation
-
-\`\`\`bash
-$ npm install
-\`\`\`
-
-## Running the app
-
-\`\`\`bash
-# development
-$ npm run start
-
-# watch mode
-$ npm run start:dev
-
-# production mode
-$ npm run start:prod
-\`\`\`
-
-## Test
-
-\`\`\`bash
-# unit tests
-$ npm run test
-
-# e2e tests
-$ npm run test:e2e
-
-# test coverage
-$ npm run test:cov
-\`\`\`
-
-## Support
-
-Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
-
-    `;
-        let message = this.converter.convert(content);
-        // console.log(
-        //     this.converter.getMetaData()
-        // )
-        return {message: message};
+    /**
+     * Собирает данные в один объект для отдачи рендеру
+     * @param text содержимое страницы
+     * @param breadcrumbs масств хлебных крошек
+     * @private
+     */
+    private build_response(text, breadcrumbs)
+    {
+        let content = this.converter.convert(text);
+        const metaData = this.converter.getMetaData();
+        let title = metaData.title;
+        let pageHeader = metaData.header;
+        return {
+            content,
+            title,
+            pageHeader,
+            breadcrumbs
+        };
     }
 }
