1ee02ef9c1b33572b9b8df551455a341c781c744
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-17 14:57:08 +0600

    Загрузка статических файлов

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 
+const fs = require('fs/promises');
+const path = require('node:path');
+
+/**
+ * Загрузка статичесикх файлов
+ */
 @Injectable()
-export class AppService {
+export class AppService
+{
+
+    constructor(){}
+
+    loadFile(fileName: string): Promise<string>
+    {
+        let filePath = path.resolve(__dirname, '..', 'static-pages', fileName);
+        return fs.readFile(filePath, 'utf8')
+            .then((text: string)=>{
+                return Promise.resolve(text)
+            })
+            .catch(err => {
+                throw new HttpException('Не удалось найти запрашиваемый материал', HttpStatus.NOT_FOUND);
+            })
+    }
 
 }
