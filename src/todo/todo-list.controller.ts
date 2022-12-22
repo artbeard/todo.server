8d6ec742414423cd75aadbf94760dc9de778df6a
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-22 16:13:25 +0600

    Добавление списка
    todo - вынос проверки аутентификации в отдельный guard

--- src/todo/todo-list.controller.ts
+++ src/todo/todo-list.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get, Post, Put, Patch, Delete, Param, Req, Body } from '@nestjs/common';
-import { Request } from 'express'
+import {Controller, Get, Post, Put, Patch, Delete, Param, Req, Res, Body, HttpException} from '@nestjs/common';
+import {Request, Response} from 'express'
 
 import { TodoListService } from './todo-list.service'
 //import { TodoItemService } from './todo-item.service'
+
+import { ListEntity } from "./list.entity";
+
+import { UserService } from "../user/user.service";
+
+
 
 interface IDoneItemDto{
     complited: boolean
@@ -13,12 +19,21 @@ interface ITodoItemDto{
     checked: boolean
 }
 
+interface ITodoItemDto{
+    component: string,
+    checked: boolean
+}
+
+
+
+
 @Controller('/api/todo/')
 export class TodoListController {
 
     constructor(
         private todoListService: TodoListService,
         //private todoItemService: TodoItemService
+        protected userService: UserService,
     ){}
 
     /**
@@ -39,6 +54,40 @@ export class TodoListController {
     @Get('list/:list_id')
     getList(@Param('list_id') list_id: number, @Req() request: Request): any {
         return this.todoListService.findOne(list_id);
+    }
+
+    @Post('list')
+    createList(
+        @Body() { title }: { title: string },
+        @Req() request: Request,
+        @Res({ passthrough: true }) response: Response): any
+    {
+        let uid = request.cookies?.uid ?? undefined;
+        let hash = request.cookies?.token ?? undefined;
+        //title
+        return new Promise(resolve => {
+            const user = this.userService.getUser(uid)
+                .then(user => {
+                    if (!this.userService.isValidUser(user, hash))
+                        throw new HttpException('Досуп запрещен', 403);
+
+                    let newList = new ListEntity();
+                    newList.userId = user.id;
+                    newList.title = title;
+                    this.todoListService.addList(newList)
+                        .then(data => {
+                            resolve({id: data.id});
+                            //return {id: data.id}
+                        })
+
+                    //this.todoListService.
+                })
+        });
+
+
+
+
+        //return {id: 122}
     }
 
 }
