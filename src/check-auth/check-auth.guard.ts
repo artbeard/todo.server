f61080b12865914fcd47c9125c5a85f411ca4550
artbeard <76746834+artbeard@users.noreply.github.com>
2022-12-24 13:51:30 +0600

    Добавлен Guard

---
+++ src/check-auth/check-auth.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class CheckAuthGuard implements CanActivate {
+
+    constructor(
+        private userService: UserService
+    ) {}
+
+    canActivate( context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const cookies = context.switchToHttp().getRequest()?.cookies;
+        if (!cookies.uid || !cookies.token)
+            return false;
+        return new Promise((resolve) => {
+            this.userService.getUser(cookies.uid)
+                .then(user => {
+                    if (this.userService.isValidUser(user, cookies.token)) {
+                        resolve(true);
+                    } else {
+                        resolve(false);
+                    }
+                })
+        })
+    }
+}
